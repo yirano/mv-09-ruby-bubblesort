9e1f4b6cea4b05159bf0842df1d7eebfc80c1111
yirano <tiffany.arellano@outlook.com>
2019-10-03 13:49:25 -0400

    Add second method
    We forgot to do write the bubble_sort_by method

--- code.rb
+++ code.rb
@@ -1,24 +1,41 @@
 def bubble_sort (array)
-
   n = array.length
-
   loop do
-
-    swaped = false
+    swapped = false
     (n-1).times do |i|
       if array[i] > array[i+1]
         array[i], array[i+1] = array[i+1], array[i]
-        swaped = true
+        swapped = true
       end
     end
-
-    break if not swaped
-
+    break if not swapped
   end 
-
   array
-
 end
 
+def bubble_sort_by(arr)
+  cont = true
+  while cont
+    for i in 0...arr.length-1
+      if yield([arr[i], arr[i+1]]) > 0
+          arr[i], arr[i+1] = arr[i+1], arr[i]
+      else
+          cont = false
+      end
+    end
+  end
+  arr
+end
+
+
+# tests
 array = [3, 7, 1, 9, 2, 8]
-print bubble_sort (array)
+p bubble_sort(array)
+
+ans = bubble_sort_by(['hi', 'bye', 'THE WORLD']) do |a, b|
+  a.length - b.length
+end
+p ans
+
+
+
